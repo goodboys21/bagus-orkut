963a0d02b75c6dac92e6e7237e20aa8fb17c2228
goodboys21 <akunceeingood5@gmail.com>
2025-04-19 11:10:21 +0700

    generateQRIS.js

--- generateQRIS.js
+++ generateQRIS.js
@@ -3,43 +3,36 @@ const fs = require("fs");
 const elxyzFile = require('./elxyzFile');
 
 function convertCRC16(str) {
-  let crc = 0xFFFF;
-  const strlen = str.length;
-
-  for (let c = 0; c < strlen; c++) {
-    crc ^= str.charCodeAt(c) << 8;
-    for (let i = 0; i < 8; i++) {
-      if (crc & 0x8000) {
-        crc = (crc << 1) ^ 0x1021;
-      } else {
-        crc = crc << 1;
-      }
+    let crc = 0xFFFF;
+    for (let c = 0; c < str.length; c++) {
+        crc ^= str.charCodeAt(c) << 8;
+        for (let i = 0; i < 8; i++) {
+            crc = (crc & 0x8000) ? (crc << 1) ^ 0x1021 : crc << 1;
+        }
     }
-  }
-
-  let hex = crc & 0xFFFF;
-  hex = ("000" + hex.toString(16).toUpperCase()).slice(-4);
-  return hex;
+    return ("000" + ((crc & 0xFFFF).toString(16).toUpperCase())).slice(-4);
 }
 
 const generateQRIS = async (codeqr) => {
-  try {
-    let qrisData = codeqr;
-    qrisData = qrisData.slice(0, -4);
-    const step1 = qrisData.replace("010211", "010212");
-    const step2 = step1.split("5802ID");
-    const result = step2[0] + "5802ID" + step2[1] + convertCRC16(step2[0] + "5802ID" + step2[1]);
+    try {
+        let qrisData = codeqr;
+        qrisData = qrisData.slice(0, -4);
+        const step1 = qrisData.replace("010211", "010212");
+        const step2 = step1.split("5802ID");
 
-    await QRCode.toFile('qr_image.png', result);
+        const result = step2[0] + "5802ID" + step2[1] + convertCRC16(step2[0] + "5802ID" + step2[1]);
 
-    const uploadedFile = await elxyzFile('qr_image.png');
-    fs.unlinkSync('qr_image.png');
+        const path = 'qr_image.png';
+        await QRCode.toFile(path, result);
 
-    return { qrImageUrl: uploadedFile.url };
-  } catch (error) {
-    console.error('Error generating and uploading QR code:', error);
-    throw error;
-  }
+        const uploadedFile = await elxyzFile(path);
+        fs.unlinkSync(path);
+
+        return { qrImageUrl: uploadedFile.fileUrl };
+    } catch (error) {
+        console.error('Error generating and uploading QR code:', error);
+        throw error;
+    }
 };
 
 module.exports = generateQRIS;
