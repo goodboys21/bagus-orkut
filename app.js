238526b05480f440f0f7515cf90bc11c57ccc494
goodboys21 <akunceeingood5@gmail.com>
2025-08-03 13:02:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json({ limit: '100mb' }));
 
 app.post('/tools/mdfup', async (req, res) => {
   const { apikey, filename, buffer } = req.body;
